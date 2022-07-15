90e45e01a64d79384f5dd48d9d5f11e3059b2e9d
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-15 18:42:25 -0500

    v0.5.0 Se integra la creación del front para las listas de tipo empresa

---
+++ views/org_empresa/lista.php
@@ -0,0 +1,51 @@
+<div class="col-md-12 info">
+    <div class="col-lg-12 content">
+        <h3 class="text-center titulo-form">Hola, Nombre Completo </h3>
+
+        <div class="col-md-12 mt-3 table-responsive-sm">
+
+            <div class="filters">
+                <div class="filter col-md-4 "></div>
+                <div class="search col-md-8 ">
+                    <input type="text form-control input">
+                </div>
+            </div>
+
+            <table class="table table-dark">
+                <thead>
+                <tr>
+                    <th scope="col">Column 1</th>
+                    <th scope="col">Column 1</th>
+                    <th scope="col">Column 1</th>
+                    <th scope="col">Column 1</th>
+                    <th scope="col">Column 1</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>@mdo</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
