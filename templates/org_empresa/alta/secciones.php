dc195a49074a217c70250f3e267ab78e0d845004
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-20 12:38:04 -0500

    v0.9.0 Se ajusta color en active

--- templates/org_empresa/alta/secciones.php
+++ templates/org_empresa/alta/secciones.php
@@ -7,7 +7,7 @@
         <div class="col-md-8">
             <h3>Alta de empresas</h3>
 
-            <button class="btn btn-default menu-lateral">
+            <button class="btn btn-default menu-lateral menu-lateral-active">
                 <img src="img/1.azul.svg" class="numero"> <span class="texto-menu-lateral">Generales</span>
 
             </button>
