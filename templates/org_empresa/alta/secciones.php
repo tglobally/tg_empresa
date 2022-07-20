fc92c10ebde1b9c378015049eadef640e07780cf
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-20 11:43:57 -0500

    v0.6.0 Se ajusta css para menu lateral

--- templates/org_empresa/alta/secciones.php
+++ templates/org_empresa/alta/secciones.php
@@ -1,21 +1,27 @@
 <div class="col-md-3 secciones">
 
-    <div class="col-md-12 int_secciones">
-        <h3>Alta de empresas</h3>
-        <br>
-        Generales
-        <br>
-        <br>
-        Generales
-        <br>
-        <br>
-        Generales
-        <br>
-        <br>
-        Generales
-        <br>
-        <br>
-        Generales
-        <br>
+    <div class="col-md-12 int_secciones ">
+        <div class="col-md-4 seccion">
+            <img src="img/1.svg" class="img-seccion">
+        </div>
+        <div class="col-md-8 menu-seccion-1">
+            <h3>Alta de empresas</h3>
+            <br>
+            Generales
+            <br>
+            <br>
+            Generales
+            <br>
+            <br>
+            Generales
+            <br>
+            <br>
+            Generales gjg gughjukhyiuy yyiu
+            <br>
+            <br>
+            Generales
+            <br>
+        </div>
+
     </div>
 </div>
