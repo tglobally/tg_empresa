dc487331217596b7a3d655395dea599a92e5ffff
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-20 12:12:42 -0500

    v0.7.0 Se integran numeros de menus laterales

--- templates/org_empresa/alta/secciones.php
+++ templates/org_empresa/alta/secciones.php
@@ -4,23 +4,16 @@
         <div class="col-md-4 seccion">
             <img src="img/1.svg" class="img-seccion">
         </div>
-        <div class="col-md-8 menu-seccion-1">
+        <div class="col-md-8">
             <h3>Alta de empresas</h3>
-            <br>
-            Generales
-            <br>
-            <br>
-            Generales
-            <br>
-            <br>
-            Generales
-            <br>
-            <br>
-            Generales gjg gughjukhyiuy yyiu
-            <br>
-            <br>
-            Generales
-            <br>
+            <button class="btn btn-default menu-lateral">
+                <img src="img/1.azul.svg" class="numero"> <span class="texto-menu-lateral">Generales</span>
+            </button>
+            <button class="btn btn-default menu-lateral">
+                <img src="img/2.gris.svg" class="numero"> <span class="texto-menu-lateral">Generales</span>
+            </button>
+
+
         </div>
 
     </div>
