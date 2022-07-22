228aeb66ee5d216bd86bf0f3f083c42a104860d4
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-22 16:49:16 -0500

    v0.31.1 Template button 1.gris

--- templates/org_empresa/alta/secciones.php
+++ templates/org_empresa/alta/secciones.php
@@ -7,10 +7,8 @@
         <div class="col-md-8">
             <h3>Alta de empresas</h3>
 
-            <button class="btn btn-default menu-lateral menu-lateral-active">
-                <?php include "templates/org_empresa/_base/buttons/_1.gris.php"; ?>
+            <?php include "templates/org_empresa/_base/buttons/1.gris.php"; ?>
 
-            </button>
             <hr class="hr-menu-lateral">
             <button class="btn btn-default menu-lateral">
                 <?php include "templates/org_empresa/_base/buttons/_2.gris.php"; ?>
--- templates/org_empresa/ubicaciones/secciones.php
+++ templates/org_empresa/ubicaciones/secciones.php
@@ -8,9 +8,7 @@
         <div class="col-md-8">
             <h3>Empresas</h3>
             <a href="index.php?seccion=org_empresa&accion=modifica&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>">
-            <button class="btn btn-default  menu-lateral">
-                <?php include "templates/org_empresa/_base/buttons/_1.gris.php"; ?>
-            </button>
+                <?php include "templates/org_empresa/_base/buttons/1.gris.php"; ?>
             </a>
             <hr class="hr-menu-lateral">
             <button class="btn btn-default menu-lateral menu-lateral-active">
