4212188ce69d2e60c5356adbd31e3b15c6fa8510
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-22 16:51:09 -0500

    v0.32.1 Template button 2.gris

--- templates/org_empresa/alta/secciones.php
+++ templates/org_empresa/alta/secciones.php
@@ -10,9 +10,7 @@
             <?php include "templates/org_empresa/_base/buttons/1.gris.php"; ?>
 
             <hr class="hr-menu-lateral">
-            <button class="btn btn-default menu-lateral">
-                <?php include "templates/org_empresa/_base/buttons/_2.gris.php"; ?>
-            </button>
+            <?php include "templates/org_empresa/_base/buttons/2.gris.php"; ?>
 
         </div>
 
