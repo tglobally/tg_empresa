8425bc4a1b69a1e45a1ba369315c83d36b9760d2
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-22 11:40:47 -0500

    v0.17.0 Se integra link para datos de ubicacion

--- templates/org_empresa/modifica/secciones.php
+++ templates/org_empresa/modifica/secciones.php
@@ -1,3 +1,4 @@
+<?php /** @var controllers\controlador_org_empresa $controlador */ ?>
 <div class="col-md-3 secciones">
 
     <div class="col-md-12 int_secciones ">
@@ -12,10 +13,11 @@
 
             </button>
             <hr class="hr-menu-lateral">
-            <button class="btn btn-default menu-lateral">
-                <img src="img/2.gris.svg" class="numero"> <span class="texto-menu-lateral">Ubicacion</span>
-
-            </button>
+            <a href="index.php?seccion=org_empresa&accion=ubicacion&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>">
+                <button class="btn btn-default menu-lateral">
+                    <img src="img/2.gris.svg" class="numero"> <span class="texto-menu-lateral">Ubicacion</span>
+                </button>
+            </a>
 
         </div>
 
