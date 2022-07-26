ba768058f8636a5d4d7cf85e4cb276217ef43056
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-07-26 16:00:34 -0500

    v0.38.2 Se agrego estilo y campos de cif

---
+++ templates/org_empresa/cif/secciones.php
@@ -0,0 +1,20 @@
+<?php /** @var controllers\controlador_org_empresa $controlador */ ?>
+<div class="col-md-3 secciones">
+
+    <div class="col-md-12 int_secciones ">
+        <div class="col-md-4 seccion">
+            <img src="img/3.svg" class="img-seccion">
+        </div>
+        <div class="col-md-8">
+            <h3>Empresas</h3>
+            <?php include "templates/org_empresa/_base/links/1.php"; ?>
+            <hr class="hr-menu-lateral">
+            <?php include "templates/org_empresa/_base/links/2.php"; ?>
+            <hr class="hr-menu-lateral">
+            <?php include "templates/org_empresa/_base/buttons/3.azul.php"; ?>
+            <hr class="hr-menu-lateral">
+            <?php include "templates/org_empresa/_base/links/4.php"; ?>
+
+        </div>
+    </div>
+</div>
---
+++ views/org_empresa/cif.php
@@ -0,0 +1,26 @@
+<?php /** @var controllers\controlador_org_empresa $controlador */ ?>
+<?php include 'templates/org_empresa/cif/secciones.php'; ?>
+<div class="col-md-9 formulario">
+    <div class="col-lg-12">
+
+        <h3 class="text-center titulo-form">Hola, Nombre Usuario </h3>
+
+        <div class="  form-main" id="form">
+            <form method="post" action="./index.php?seccion=org_empresa&accion=modifica_cif&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+                <?php echo $controlador->inputs->select->cat_sat_regimen_fiscal_id; ?>
+                <?php echo $controlador->inputs->fecha_inicio_operaciones; ?>
+                <?php echo $controlador->inputs->fecha_ultimo_cambio_sat; ?>
+                <?php echo $controlador->inputs->email_sat; ?>
+                <div class="buttons col-md-12">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Guarda</button>
+                    </div>
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Siguiente</button>
+                    </div>
+                    
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
