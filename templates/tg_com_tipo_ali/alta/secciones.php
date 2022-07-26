67a8f3855f8079d3fa597b59ebe755ed629e6d8a
javier <javier.flores@ivitec.com.mx>
2022-07-26 15:23:58 -0500

    v0.37.2 falto agregar un fichero de alta

---
+++ templates/tg_com_tipo_ali/alta/secciones.php
@@ -0,0 +1,23 @@
+<div class="col-md-3 secciones">
+
+    <div class="col-md-12 int_secciones ">
+        <div class="col-md-4 seccion">
+            <img src="img/1.svg" class="img-seccion">
+        </div>
+        <div class="col-md-8">
+            <h3>Alta de Sucursal</h3>
+
+            <button class="btn btn-default menu-lateral menu-lateral-active">
+                <img src="img/1.azul.svg" class="numero"> <span class="texto-menu-lateral">Generales</span>
+
+            </button>
+            <hr class="hr-menu-lateral">
+            <button class="btn btn-default menu-lateral">
+                <img src="img/2.gris.svg" class="numero"> <span class="texto-menu-lateral">Ubicacion</span>
+
+            </button>
+
+        </div>
+
+    </div>
+</div>
