99e3c69c49cb6fd7d1da0622cfb1893e7d229f99
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-26 18:19:14 -0500

    v.44.2 Se integran los campos logo y pagina web en identidad

--- views/org_empresa/identidad.php
+++ views/org_empresa/identidad.php
@@ -7,7 +7,19 @@
         <h3 class="text-center titulo-form">Hola, Nombre Usuario </h3>
 
         <div class="  form-main" id="form">
+            <form method="post" action="./index.php?seccion=org_empresa&accion=modifica_identidad&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+                <?php echo $controlador->inputs->logo; ?>
+                <?php echo $controlador->inputs->pagina_web ?>
+                <div class="buttons col-md-12">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Guarda</button>
+                    </div>
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Siguiente</button>
+                    </div>
 
+                </div>
+            </form>
         </div>
     </div>
 </div>
