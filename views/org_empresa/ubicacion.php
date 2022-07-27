695400a331119e4fec43654ae3c89a01232de526
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-27 12:15:38 -0500

    v.46.2 Se integran inputs en ubicacion

--- views/org_empresa/ubicacion.php
+++ views/org_empresa/ubicacion.php
@@ -9,6 +9,15 @@
         <div class="  form-main" id="form">
             <form method="post" action="./index.php?seccion=org_empresa&accion=modifica_ubicacion&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
                 <?php echo $controlador->inputs->select->dp_pais_id; ?>
+                <?php echo $controlador->inputs->select->dp_estado_id; ?>
+                <?php echo $controlador->inputs->select->dp_municipio_id; ?>
+                <?php echo $controlador->inputs->select->dp_cp_id; ?>
+                <?php echo $controlador->inputs->select->dp_colonia_postal_id; ?>
+                <?php echo $controlador->inputs->select->dp_calle_pertenece_id; ?>
+                <?php echo $controlador->inputs->exterior; ?>
+                <?php echo $controlador->inputs->interior; ?>
+                <?php echo $controlador->inputs->select->dp_calle_pertenece_entre1_id; ?>
+                <?php echo $controlador->inputs->select->dp_calle_pertenece_entre2_id; ?>
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
                         <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Guarda</button>
