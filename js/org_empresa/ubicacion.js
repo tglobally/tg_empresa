404e3322362a68d9cbac8d1722b61e93d600a83f
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-27 13:23:39 -0500

    v.48.2 Se integra js ubicacion

---
+++ js/org_empresa/ubicacion.js
@@ -0,0 +1,135 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    const regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+function integra_new_option(container, descripcion, value){
+    let new_option =new_option_sl(descripcion,value);
+    $(new_option).appendTo(container);
+}
+
+
+
+function new_option_sl(descripcion,value){
+    return "<option value =" + value + ">" + descripcion + "</option>";
+}
+let session_id = getParameterByName('session_id');
+let dp_pais_id = -1;
+let dp_estado_id = -1;
+let dp_municipio_id = -1;
+let dp_cp_id = -1;
+let dp_colonia_postal_id = -1;
+
+let sl_dp_estado_id = $("#dp_estado_id");
+let sl_dp_pais_id = $("#dp_pais_id");
+let sl_dp_municipio_id = $("#dp_municipio_id");
+let sl_dp_cp_id = $("#dp_cp_id");
+let sl_dp_colonia_postal_id = $("#dp_colonia_postal_id");
+let sl_dp_calle_pertenece_id = $("#dp_calle_pertenece_id");
+let sl_dp_calle_pertenece_entre1_id = $("#dp_calle_pertenece_entre1_id");
+let sl_dp_calle_pertenece_entre2_id = $("#dp_calle_pertenece_entre2_id");
+
+sl_dp_pais_id.change(function(){
+    dp_pais_id = $(this).val();
+
+    let url = "index.php?seccion=dp_estado&ws=1&accion=get_estado&dp_pais_id="+dp_pais_id+"&session_id="+session_id;
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
+
+        $.each(data.registros, function( index, dp_estado ) {
+            integra_new_option("#dp_estado_id",dp_estado.dp_pais_descripcion+' '+dp_estado.dp_estado_descripcion,dp_estado.dp_estado_id);
+        });
+        sl_dp_estado_id.selectpicker('refresh');
+    }).fail(function (jqXHR, textStatus, errorThrown){
+        console.log(url);
+        console.log("Error: "+ textStatus +" "+ errorThrown+" "+jqXHR+" ");
+    });
+});
+
+sl_dp_estado_id.change(function(){
+    dp_estado_id = $(this).val();
+    let url = "index.php?seccion=dp_municipio&ws=1&accion=get_municipio&dp_estado_id="+dp_estado_id+"&session_id="+session_id;
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
+        console.log(data);
+        $.each(data.registros, function( index, dp_municipio ) {
+            integra_new_option("#dp_municipio_id",dp_municipio.dp_estado_descripcion+' '+dp_municipio.dp_municipio_descripcion,dp_municipio.dp_municipio_id);
+        });
+        sl_dp_municipio_id.selectpicker('refresh');
+    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
+        alert('Error al ejecutar');
+        console.log("The following error occured: "+ textStatus +" "+ errorThrown);
+    });
+});
+
+
+sl_dp_municipio_id.change(function(){
+    dp_municipio_id = $(this).val();
+    let url = "index.php?seccion=dp_cp&ws=1&accion=get_cp&dp_municipio_id="+dp_municipio_id+"&session_id="+session_id;
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
+        console.log(data);
+        $.each(data.registros, function( index, dp_cp ) {
+            integra_new_option("#dp_cp_id",dp_cp.dp_municipio_descripcion+' '+dp_cp.dp_cp_descripcion,dp_cp.dp_cp_id);
+        });
+        sl_dp_cp_id.selectpicker('refresh');
+    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
+        alert('Error al ejecutar');
+    });
+});
+
+sl_dp_cp_id.change(function(){
+    dp_cp_id = $(this).val();
+    let url = "index.php?seccion=dp_colonia_postal&ws=1&accion=get_colonia_postal&dp_cp_id="+dp_cp_id+"&session_id="+session_id;
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
+        console.log(data);
+        $.each(data.registros, function( index, dp_colonia_postal ) {
+            integra_new_option("#dp_colonia_postal_id",dp_colonia_postal.dp_colonia_descripcion+' '+dp_colonia_postal.dp_cp_descripcion,dp_colonia_postal.dp_colonia_postal_id);
+        });
+        sl_dp_colonia_postal_id.selectpicker('refresh');
+    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
+        alert('Error al ejecutar');
+        console.log(url);
+    });
+});
+
+sl_dp_colonia_postal_id.change(function(){
+    dp_colonia_postal_id = $(this).val();
+    let url = "index.php?seccion=dp_calle_pertenece&ws=1&accion=get_calle_pertenece&dp_colonia_postal_id="+dp_colonia_postal_id+"&session_id="+session_id;
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
+        console.log(data);
+        $.each(data.registros, function( index, dp_calle_pertenece ) {
+            integra_new_option("#dp_calle_pertenece_id",dp_calle_pertenece.dp_colonia_descripcion+' '+dp_calle_pertenece.dp_cp_descripcion+' '+dp_calle_pertenece.dp_calle_descripcion,dp_calle_pertenece.dp_calle_pertenece_id);
+            integra_new_option("#dp_calle_pertenece_entre1_id",dp_calle_pertenece.dp_colonia_descripcion+' '+dp_calle_pertenece.dp_cp_descripcion+' '+dp_calle_pertenece.dp_calle_descripcion,dp_calle_pertenece.dp_calle_pertenece_id);
+            integra_new_option("#dp_calle_pertenece_entre2_id",dp_calle_pertenece.dp_colonia_descripcion+' '+dp_calle_pertenece.dp_cp_descripcion+' '+dp_calle_pertenece.dp_calle_descripcion,dp_calle_pertenece.dp_calle_pertenece_id);
+        });
+        sl_dp_calle_pertenece_id.selectpicker('refresh');
+        sl_dp_calle_pertenece_entre1_id.selectpicker('refresh');
+        sl_dp_calle_pertenece_entre2_id.selectpicker('refresh');
+    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
+        alert('Error al ejecutar');
+        console.log(url);
+    });
+});
+
+
+
+
