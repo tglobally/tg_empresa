9fcbd7c4523df325730da28c166addae37f5091a
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-29 11:34:00 -0500

    V.0.56.1 Se genera formulario para seccion actividad economica

--- templates/org_porcentaje_act_economica/alta/secciones.php
+++ templates/org_porcentaje_act_economica/alta/secciones.php
@@ -1,9 +1,12 @@
-<?php /** @var controllers\controlador_org_empresa $controlador */?>
+<?php /** @var controllers\controlador_org_porcentaje_act_economica $controlador */?>
 <div class="col-md-3 secciones">
 
     <div class="col-md-12 int_secciones ">
         <div class="col-md-12 ">
-
+            <h4 class="sub_text_datos">RFC:</h4>
+            <h5 class="text_datos"><?php echo $controlador->rfc;?></h5>
+            <h4 class="sub_text_datos">Razón social:</h4>
+            <h5 class="text_datos"><?php echo $controlador->razon_social;?></h5>
         </div>
         <div class="col-md-4 seccion">
             <img src="img/7.svg" class="img-seccion">
@@ -24,5 +27,6 @@
             <hr class="hr-menu-lateral">
             <?php include "templates/org_empresa/_base/buttons/7.azul.php"; ?>
         </div>
+
     </div>
 </div>
---
+++ views/org_porcentaje_act_economica/alta.php
@@ -0,0 +1,56 @@
+<?php /** @var controllers\controlador_org_sucursal $controlador */ ?>
+<?php include 'templates/org_porcentaje_act_economica/alta/secciones.php'; ?>
+<div class="col-md-9 formulario">
+    <div class="col-lg-12">
+
+        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+
+        <div class="  form-main" id="form">
+            <form method="post" action="./index.php?seccion=org_porcentaje_act_economica&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
+                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->codigo_bis; ?>
+                <?php echo $controlador->inputs->select->org_empresa_id; ?>
+                <?php echo $controlador->inputs->select->cat_sat_actividad_economica_id; ?>
+                <?php echo $controlador->inputs->porcentaje; ?>
+
+                <div class="buttons col-md-12">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Guarda</button>
+                    </div>
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Siguiente</button>
+                    </div>
+
+                </div>
+
+                <div class="cont_tabla_sucursal col-md-12">
+                    <table class="table table-dark">
+                        <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Codigo</th>
+                            <th scope="col">Empresa</th>
+                            <th scope="col">Actividad Economica</th>
+                            <th scope="col">Porcentaje</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        foreach ($controlador->sucursales as $registro){
+                            echo "<tr>";
+                            echo "<td>$registro[org_sucursal_id]</td>";
+                            echo "<td>$registro[org_sucursal_codigo]</td>";
+                            echo "<td>$registro[org_sucursal_fecha_inicio_operaciones]</td>";
+                            echo "<td>$registro[direccion]</td>";
+                            echo "<td>$registro[org_sucursal_telefono_1]</td>";
+                            echo "<td>$registro[org_sucursal_telefono_2]</td>";
+                            echo "<td>$registro[org_sucursal_telefono_3]</td>";
+                            echo "</tr>";
+                        } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
