4673440cf7290939a8fad57a397e722493d47da5
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-17 13:03:37 -0500

    v0.93.8 Se implementa view sucursales

--- views/org_empresa/sucursales.php
+++ views/org_empresa/sucursales.php
@@ -1,4 +1,4 @@
-<?php /** @var controllers\controlador_org_sucursal $controlador */ ?>
+<?php /** @var tglobally\tg_empresa\controllers\controlador_org_empresa $controlador */ ?>
 <?php include 'templates/org_sucursal/alta/secciones.php'; ?>
 <div class="col-md-9 formulario">
     <div class="col-lg-12">
@@ -6,22 +6,29 @@
         <h3 class="text-center titulo-form">Hola, Nombre Usuario </h3>
 
         <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=org_sucursal&accion=alta_bd&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
+            <form method="post" action="<?php echo $controlador->link_org_sucursal_alta_bd; ?>" class="form-additional">
+
                 <?php echo $controlador->inputs->select->org_empresa_id; ?>
                 <?php echo $controlador->inputs->codigo; ?>
                 <?php echo $controlador->inputs->codigo_bis; ?>
+
                 <?php echo $controlador->inputs->fecha_inicio_operaciones; ?>
+
                 <?php echo $controlador->inputs->select->dp_pais_id; ?>
                 <?php echo $controlador->inputs->select->dp_estado_id; ?>
                 <?php echo $controlador->inputs->select->dp_municipio_id; ?>
                 <?php echo $controlador->inputs->select->dp_cp_id; ?>
                 <?php echo $controlador->inputs->select->dp_colonia_postal_id; ?>
                 <?php echo $controlador->inputs->select->dp_calle_pertenece_id; ?>
+
                 <?php echo $controlador->inputs->exterior; ?>
                 <?php echo $controlador->inputs->interior; ?>
+
                 <?php echo $controlador->inputs->telefono_1; ?>
                 <?php echo $controlador->inputs->telefono_2; ?>
                 <?php echo $controlador->inputs->telefono_3; ?>
+
+
                 <div class="buttons col-md-12">
                     <div class="col-md-6 btn-ancho">
                         <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Guarda</button>
@@ -36,28 +43,46 @@
                     <table class="table table-dark">
                         <thead>
                         <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Codigo</th>
-                            <th scope="col">Fecha Inicio Operacion</th>
-                            <th scope="col">Dirección</th>
-                            <th scope="col">Telefono 1</th>
-                            <th scope="col">Telefono 2</th>
-                            <th scope="col">Telefono 3</th>
+                            <th>Id</th>
+                            <th>Codigo</th>
+                            <th>Tipo</th>
+                            <th>Descripcion</th>
+                            <th>Fecha Inicio</th>
+                            <th>Calle</th>
+                            <th>Ext</th>
+                            <th>Int</th>
+                            <th>CP</th>
+                            <th>Mun</th>
+                            <th>Edo</th>
+                            <th>Tel</th>
+                            <th>Serie</th>
+                            <th>Ver</th>
+                            <th>Modifica</th>
+                            <th>Elimina</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <?php
-                        foreach ($controlador->sucursales as $registro){
-                            echo "<tr>";
-                            echo "<td>$registro[org_sucursal_id]</td>";
-                            echo "<td>$registro[org_sucursal_codigo]</td>";
-                            echo "<td>$registro[org_sucursal_fecha_inicio_operaciones]</td>";
-                            echo "<td>$registro[direccion]</td>";
-                            echo "<td>$registro[org_sucursal_telefono_1]</td>";
-                            echo "<td>$registro[org_sucursal_telefono_2]</td>";
-                            echo "<td>$registro[org_sucursal_telefono_3]</td>";
-                            echo "</tr>";
-                        } ?>
+                        <?php foreach ($controlador->sucursales->registros as $sucursal){ ?>
+                            <tr>
+                                <td><?php echo $sucursal['org_sucursal_id']; ?></td>
+                                <td><?php echo $sucursal['org_sucursal_codigo']; ?></td>
+                                <td><?php echo $sucursal['org_tipo_sucursal_descripcion']; ?></td>
+                                <td><?php echo $sucursal['org_sucursal_descripcion']; ?></td>
+                                <td><?php echo $sucursal['org_sucursal_fecha_inicio_operaciones']; ?></td>
+                                <td><?php echo $sucursal['dp_calle_descripcion']; ?></td>
+                                <td><?php echo $sucursal['org_sucursal_exterior']; ?></td>
+                                <td><?php echo $sucursal['org_sucursal_interior']; ?></td>
+                                <td><?php echo $sucursal['dp_cp_descripcion']; ?></td>
+                                <td><?php echo $sucursal['dp_municipio_descripcion']; ?></td>
+                                <td><?php echo $sucursal['dp_estado_descripcion']; ?></td>
+                                <td><?php echo $sucursal['org_sucursal_telefono_1']; ?></td>
+                                <td><?php echo $sucursal['org_sucursal_serie']; ?></td>
+                                <td><?php echo $sucursal['link_ve']; ?></td>
+                                <td><?php echo $sucursal['link_modifica']; ?></td>
+                                <td><?php echo $sucursal['link_elimina']; ?></td>
+
+                            </tr>
+                        <?php } ?>
                         </tbody>
                     </table>
                 </div>
