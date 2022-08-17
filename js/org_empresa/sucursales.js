88479d0fa6ea9b9c43b8a7ed8edd549b0a0ec483
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-17 13:07:21 -0500

    v0.94.8 Se implementa js sucursales

---
+++ js/org_empresa/sucursales.js
@@ -0,0 +1,11 @@
+function getAbsolutePath() {
+    var loc = window.location;
+    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+}
+let url = getAbsolutePath();
+
+let direcciones_js = url+'js/direcciones.js';
+
+document.write('<script src="'+direcciones_js+'"></script>');
+let session_id = getParameterByName('session_id');
