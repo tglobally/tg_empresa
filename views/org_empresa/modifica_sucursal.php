bc7ba2cb5279b80abca141f9cec51878270fd481
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-18 14:33:13 -0500

    v0.105.8 Se integra upd view sucursal

---
+++ views/org_empresa/modifica_sucursal.php
@@ -0,0 +1,51 @@
+<?php /** @var tglobally\tg_empresa\controllers\controlador_org_empresa $controlador */
+
+use config\views; ?>
+<?php include 'templates/org_sucursal/alta/secciones.php'; ?>
+
+<div class="col-md-9 formulario">
+    <div class="col-lg-12">
+        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+        <div class="  form-main" id="form">
+            <form method="post" action="<?php echo $controlador->link_org_sucursal_modifica_bd; ?>" class="form-additional">
+                <div class="col-lg-12">
+                    <?php echo $controlador->inputs->select->org_empresa_id; ?>
+                    <?php echo $controlador->inputs->razon_social; ?>
+                </div>
+                <div class="col-lg-12">
+
+                    <?php echo $controlador->inputs->org_sucursal_id; ?>
+                    <?php echo $controlador->inputs->org_sucursal_codigo; ?>
+                    <?php echo $controlador->inputs->org_sucursal_codigo_bis; ?>
+                    <?php echo $controlador->inputs->org_sucursal_descripcion; ?>
+                    <?php echo $controlador->inputs->org_sucursal_tipo_sucursal_descricpion; ?>
+                    <?php echo $controlador->inputs->org_sucursal_serie; ?>
+                    <?php echo $controlador->inputs->org_sucursal_fecha_inicio_operaciones; ?>
+                    <?php echo $controlador->inputs->org_sucursal_dp_estado_id; ?>
+                    <?php echo $controlador->inputs->org_sucursal_dp_municipio_id; ?>
+                    <?php echo $controlador->inputs->org_sucursal_dp_cp_id; ?>
+                    <?php echo $controlador->inputs->org_sucursal_dp_colonia_postal_id; ?>
+                    <?php echo $controlador->inputs->org_sucursal_dp_calle_pertenece_id; ?>
+                    <?php echo $controlador->inputs->org_sucursal_exterior; ?>
+                    <?php echo $controlador->inputs->org_sucursal_interior; ?>
+                    <?php echo $controlador->inputs->org_sucursal_telefono_1; ?>
+                    <?php echo $controlador->inputs->org_sucursal_telefono_2; ?>
+                    <?php echo $controlador->inputs->org_sucursal_telefono_3; ?>
+                    <?php if($controlador->muestra_btn_upd){ ?>
+                        <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
+                    <?php } ?>
+                </div>
+            </form>
+        </div>
+
+    </div>
+    <br>
+
+
+
+</div>
+
+
+
+
+
