bf88229c248d684dd0f3296959af20c8f4f7fc5b
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-19 14:07:54 -0500

    v0.111.8 Se ajusta salida btns css

--- css/base.php
+++ css/base.php
@@ -1,9 +1,14 @@
 <?php
 use config\generales;
+use config\views;
+
 $url_base = (new generales())->url_base;
+$url_template = (new views())->url_assets;
+$url_template .= 'css/';
+
 ?>
 <style>
     @import "<?php echo $url_base; ?>css/secciones.css";
     @import "<?php echo $url_base; ?>css/forms.css";
-    @import "<?php echo $url_base; ?>css/botones.css";
+    @import "<?php echo $url_template; ?>botones.css";
 </style>
