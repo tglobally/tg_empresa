2fb05ceaf4a66456f84362ce642cee0bd72dbae3
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-23 14:50:02 -0500

    v0.125.8 Se integra en parent numbers

--- templates/numeros/number.php
+++
@@ -1,6 +0,0 @@
-<?php
-/** @var string $number */
-use config\views;
-
-$url_assets = (new views())->url_assets; ?>
-<img src="<?php echo $url_assets."img/numeros/$number.svg"; ?>" class="numero">
--- templates/org_empresa/_base/buttons/_4.azul.php
+++ templates/org_empresa/_base/buttons/_4.azul.php
@@ -1,3 +1,4 @@
+<?php use config\views; ?>
 <?php $number = '4.azul'; ?>
-<?php include "templates/numeros/number.php"; ?>
+<?php include (new views())->ruta_templates."number.php"; ?>
 <?php include "templates/org_empresa/_base/texto_menu_lateral/contacto.php"; ?>
