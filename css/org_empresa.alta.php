7386ee5dc83196ee4f29a9f0c9d1bf8f355dbf20
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-23 13:22:14 -0500

    v0.119.8 Se implementa simnplificacion de fonts

--- css/org_empresa.alta.php
+++ css/org_empresa.alta.php
@@ -5,6 +5,3 @@ use config\views;
 $ruta_template_base = (new views())->ruta_template_base;
 include $ruta_template_base.'assets/css/_base_css.php';
 ?>
-<style>
-    @import "<?php echo $url_template; ?>fonts.css";
-</style>
--- css/org_empresa.contacto.php
+++ css/org_empresa.contacto.php
@@ -5,9 +5,7 @@ use config\views;
 $ruta_template_base = (new views())->ruta_template_base;
 include $ruta_template_base.'assets/css/_base_css.php';
 ?>
-<style>
-    @import "<?php echo $url_template; ?>fonts.css";
-</style>
+
 
 <style>
 .label-alerta-oculto {
