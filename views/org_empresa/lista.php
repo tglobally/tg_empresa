742a87a71c36f6899a30e73dcdf26fdf89ace035
[user] <angel.martinez@ivitec.com.mx>
2022-09-27 15:38:37 -0500

    v0.114.9 Se ajusta view lista empresas

--- views/org_empresa/lista.php
+++ views/org_empresa/lista.php
@@ -12,61 +12,39 @@ $url_icons = (new views())->url_icons;
     <div class="col-lg-12 content">
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h3>
 
-        <div class="col-md-12 mt-3 table-responsive-sm">
+        <div class="lista">
+            <div class="card">
 
-            <div class="filters">
-                <div class="filter col-md-4 acciones_filter">
-                    <a class="icon_xls_lista">
-                        <img src="<?php echo $url_icons; ?>icon_xls.png">
-                    </a>
-                    <a class="icon_atras_lista">
-                        <img src="<?php echo $url_icons; ?>icon_pag_atras.svg">
-                    </a>
-                    <p class="paginador">3 de 35</p>
-                    <a class="icon_adelante_lista">
-                        <img src="<?php echo $url_icons; ?>icon_pag_adelante.svg">
-                    </a>
-                </div>
-                <div class="search col-md-8 input_search">
-                    <input type="text form-control input">
-                    <img class="input_icon" src="<?php echo $url_icons; ?>icon_lupa.svg">
+                <div class="card-body">
+                    <div class="cont_tabla_sucursal  col-md-12">
+                        <table class="table ">
+                            <thead>
+                            <tr>
+                                <th data-breakpoints="xs sm md" data-type="html" >Id</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >RFC</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Razón Social</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Nombre Comercial</th>
+                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Modifica</th>
+                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Elimina</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($controlador->registros as $registro){?>
+                                <tr>
+                                    <td><?php echo $registro->org_empresa_id; ?></td>
+                                    <td><?php echo $registro->org_empresa_rfc; ?></td>
+                                    <td><?php echo $registro->org_empresa_razon_social; ?></td>
+                                    <td><?php echo $registro->org_empresa_nombre_comercial; ?></td>
+                                    <td><a class="btn btn-warning " href="<?php echo $registro->link_modifica; ?>">Modifica</a></td>
+                                    <td><a class="btn btn-danger " href="<?php echo $registro->link_elimina; ?>">Elimina</a></td>
+                                </tr>
+                            <?php } ?>
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-
-            <table class="table table-dark">
-                <thead>
-                <tr>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">Id</th>
-                    <th scope="col">RFC</th>
-                    <th scope="col">Razon Social</th>
-                    <th scope="col">Nombre comercial</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($controlador->registros as $registro){
-
-
-                    ?>
-                <tr>
-                    <th class="colum_accion" scope="row">
-                        <a class="icon_modifica_lista" href="<?php echo $registro->link_modifica; ?>">
-                            <img src="<?php echo $url_icons; ?>icon_modifica.svg">
-                        </a>
-                        <a class="icon_elimina_lista" href="<?php echo $registro->link_elimina_bd; ?>">
-                            <img src="<?php echo $url_icons; ?>icon_elimina.svg">
-                        </a>
-
-                    </th>
-                    <th><?php echo $registro->org_empresa_id; ?></th>
-                    <th><?php echo $registro->org_empresa_rfc; ?></th>
-                    <th><?php echo $registro->org_empresa_razon_social; ?></th>
-                    <th><?php echo $registro->org_empresa_nombre_comercial; ?></th>
-                </tr>
-                <?php } ?>
-
-                </tbody>
-            </table>
         </div>
 
     </div>
