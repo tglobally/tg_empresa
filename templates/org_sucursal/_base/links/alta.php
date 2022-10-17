285f7f86c69422089104faac4c217e9908065c5c
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-17 11:56:35 -0500

    v0.127.9 Se integra lista org_sucursal

---
+++ templates/org_sucursal/_base/links/alta.php
@@ -0,0 +1,4 @@
+<?php /** @var \tglobally\tg_empleado\controllers\controlador_em_empleado $controlador */ ?>
+<a href="index.php?seccion=<?php echo $controlador->tabla; ?>&accion=alta&session_id=<?php echo $controlador->session_id; ?>">
+    <?php include "templates/org_sucursal/_base/buttons/1.azul.alta.php"; ?>
+</a>
---
+++ views/org_sucursal/lista.php
@@ -0,0 +1,50 @@
+<?php /** @var \tglobally\tg_empleado\controllers\controlador_em_empleado  $controlador */ ?>
+<?php
+
+use config\views;
+$url_icons = (new views())->url_icons;
+?>
+
+<?php include 'templates/org_sucursal/lista/secciones.php'; ?>
+
+<div class="col-md-9 info-lista">
+    <div class="col-lg-12 content">
+        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h3>
+
+        <div class="lista">
+            <div class="card">
+
+                <div class="card-body">
+                    <div class="cont_tabla_sucursal  col-md-12">
+                        <table class="table ">
+                            <thead>
+                            <tr>
+                                <th data-breakpoints="xs sm md" data-type="html" >Id</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Codigo</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Descripcion</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Alias</th>
+                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Modifica</th>
+                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Elimina</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($controlador->registros as $registro){?>
+                                <tr>
+                                    <td><?php echo $registro->org_sucursal_id; ?></td>
+                                    <td><?php echo $registro->org_sucursal_codigo; ?></td>
+                                    <td><?php echo $registro->org_sucursal_descripcion; ?></td>
+                                    <td><?php echo $registro->org_sucursal_alias; ?></td>
+                                    <td><a class="btn btn-warning " href="<?php echo $registro->link_modifica; ?>">Modifica</a></td>
+                                    <td><a class="btn btn-danger " href="<?php echo $registro->link_elimina_bd; ?>">Elimina</a></td>
+                                </tr>
+                            <?php } ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
