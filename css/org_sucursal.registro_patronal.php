105e1ccbb067773ddd905aedd2a756c05d30e06c
[user] <angel.martinez@ivitec.com.mx>
2022-11-08 16:23:19 -0600

    v0.136.10 Fix registro patronal selects

--- css/org_sucursal.registro_patronal.php
+++ css/org_sucursal.registro_patronal.php
@@ -5,25 +5,13 @@ use config\views;
 $ruta_template_base = (new views())->ruta_template_base;
 include $ruta_template_base.'assets/css/_base_css.php';
 ?>
-
-
 <style>
-.cont_tabla_sucursal{
-    margin-top: 20px;
-}
-
-.table-dark thead tr{
-    background-color: #F1F2F6;
-    font-family: Helvetica;
-}
-
-.table-dark thead tr th{
-    font-family: Helvetica;
-}
-
-.table-dark thead tr td{
-    font-family: Helvetica;
-}
-
-
+    .form-control{
+        border-radius: 10px !important;
+    }
+    .color-secondary{
+        background: #f8f8f8 !important;
+    }
 </style>
+
+
