73abeb01fcfe1336a03c6376477d892723395979
[user] <angel.martinez@ivitec.com.mx>
2022-11-09 18:29:09 -0600

    v0.137.12 Se ajusta link registro patronal en sucursales

--- templates/org_sucursal/_base/links/registro_patronal.php
+++ templates/org_sucursal/_base/links/registro_patronal.php
@@ -1,4 +1,4 @@
 <?php /** @var \tglobally\tg_empresa\controllers\controlador_org_sucursal $controlador */ ?>
-<a href="index.php?seccion=<?php echo $controlador->tabla; ?>&accion=registro_patronal&session_id=<?php echo $controlador->session_id; ?>">
+<a href="index.php?seccion=<?php echo $controlador->tabla; ?>&accion=registro_patronal&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>">
     <?php include "templates/org_sucursal/_base/buttons/2.gris.registro_patronal.php"; ?>
 </a>
