8e619d1e8cf209975202e50522f0a6ff6cc2c271
[user] <angel.martinez@ivitec.com.mx>
2022-12-13 16:55:33 -0600

    v0.146.13 Se integra tipo jornada nom

---
+++ controllers/controlador_cat_sat_tipo_jornada_nom.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace tglobally\tg_empresa\controllers;
+use PDO;
+use stdClass;
+use tglobally\template_tg\html;
+
+
+class controlador_cat_sat_tipo_jornada_nom extends \gamboamartin\cat_sat\controllers\controlador_cat_sat_tipo_jornada_nom {
+
+    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+
+        $html_base = new html();
+        parent::__construct( link: $link, html: $html_base, paths_conf: $paths_conf);
+        $this->titulo_lista = 'Tipo jornada';
+
+        $this->sidebar['lista']['titulo'] = "Tipos de jornada";
+        $this->sidebar['lista']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_seccion_active: true,
+                menu_lateral_active: true));
+
+        $this->sidebar['alta']['titulo'] = "Alta Tipo Jornada";
+        $this->sidebar['alta']['stepper_active'] = true;
+        $this->sidebar['alta']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_lateral_active: true));
+
+        $this->sidebar['modifica']['titulo'] = "Modifica Tipo Jornada";
+        $this->sidebar['modifica']['stepper_active'] = true;
+        $this->sidebar['modifica']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_alta,menu_lateral_active: true));
+
+
+    }
+
+    public function menu_item(string $menu_item_titulo, string $link, bool $menu_seccion_active = false,
+                              bool $menu_lateral_active = false): array
+    {
+        $menu_item = array();
+        $menu_item['menu_item'] = $menu_item_titulo;
+        $menu_item['menu_seccion_active'] = $menu_seccion_active;
+        $menu_item['link'] = $link;
+        $menu_item['menu_lateral_active'] = $menu_lateral_active;
+
+        return $menu_item;
+    }
+
+
+
+}
