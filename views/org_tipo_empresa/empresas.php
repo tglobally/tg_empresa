6c5b05d0075aff1cc695f687cb826f471b307d63
[user] <angel.martinez@ivitec.com.mx>
2023-03-07 20:56:59 -0600

    v0.160.19 Fix inputs empresa desde tipo empresa

--- views/org_tipo_empresa/empresas.php
+++ views/org_tipo_empresa/empresas.php
@@ -11,11 +11,32 @@
         <div class="  form-main" id="form">
             <form method="post" action="<?php echo $controlador->link_org_empresa_alta_bd;?>" class="form-additional">
 
-                <?php echo $controlador->inputs->select->org_tipo_empresa_id; ?>
+                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->rfc; ?>
+                <?php echo $controlador->inputs->razon_social; ?>
+                <?php echo $controlador->inputs->nombre_comercial; ?>
+
+                <?php echo $controlador->inputs->email_sat; ?>
+                <?php echo $controlador->inputs->fecha_inicio_operaciones; ?>
+                <?php echo $controlador->inputs->fecha_ultimo_cambio_sat; ?>
+
                 <?php echo $controlador->inputs->select->cat_sat_regimen_fiscal_id; ?>
-                <?php echo $controlador->inputs->org_empresa_rfc; ?>
-                <?php echo $controlador->inputs->org_empresa_razon_social; ?>
-                <?php echo $controlador->inputs->org_empresa_nombre_comercial; ?>
+                <?php echo $controlador->inputs->select->dp_pais_id; ?>
+                <?php echo $controlador->inputs->select->dp_estado_id; ?>
+                <?php echo $controlador->inputs->select->dp_municipio_id; ?>
+                <?php echo $controlador->inputs->select->dp_cp_id; ?>
+                <?php echo $controlador->inputs->select->dp_colonia_postal_id; ?>
+                <?php echo $controlador->inputs->select->dp_calle_pertenece_id; ?>
+                <?php echo $controlador->inputs->select->dp_calle_pertenece_entre1_id; ?>
+                <?php echo $controlador->inputs->select->dp_calle_pertenece_entre2_id; ?>
+                <?php echo $controlador->inputs->select->org_tipo_empresa_id; ?>
+
+                <?php echo $controlador->inputs->exterior; ?>
+                <?php echo $controlador->inputs->interior; ?>
+
+                <?php echo $controlador->inputs->telefono_1; ?>
+                <?php echo $controlador->inputs->telefono_2; ?>
+                <?php echo $controlador->inputs->telefono_3; ?>
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
                         <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="modifica">Guarda</button>
