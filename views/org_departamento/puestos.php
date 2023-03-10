1ad1bb421509ac8d1a5ee779ff52752008f81c58
[user] <angel.martinez@ivitec.com.mx>
2023-03-10 17:13:28 -0600

    v0.160.20 Fix puestos desde departamento

--- views/org_departamento/puestos.php
+++ views/org_departamento/puestos.php
@@ -14,6 +14,10 @@
                 <?php echo $controlador->inputs->select->org_tipo_puesto_id; ?>
                 <?php echo $controlador->inputs->org_puesto_descripcion; ?>
 
+                <?php echo $controlador->inputs->hidden_row_id; ?>
+                <?php echo $controlador->inputs->hidden_seccion_retorno; ?>
+                <?php echo $controlador->inputs->hidden_id_retorno; ?>
+
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
                         <button type="submit" class="btn btn-info btn-guarda col-md-12 " value="puestos">Guarda</button>
